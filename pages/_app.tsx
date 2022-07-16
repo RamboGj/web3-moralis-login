409d44f0502e08fb0f40591905c4a8f063560b3f
João Rambo <jpramboguanabara@hotmail.com>
2022-07-16 20:15:24 -0300

    login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {MoralisProvider} from "react-moralis"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      appId={'gLzpYz7M9hFQmvfmawfLiLZ3XHMNR73Z7iOfea0g'}
+      serverUrl={'https://nk6crv5g5fld.usemoralis.com:2053/server'}
+    >
+      {
+        <Component {...pageProps} />
+      }
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
